9b8b75f1c4a7539cb361cec50db9857a1447b282
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-11-09 17:48:30 -0300

    feat: add coordinate agent

---
+++ lib/coordinate.ex
@@ -0,0 +1,31 @@
+defmodule IslandsEngine.Coordinate do
+  alias IslandsEngine.Coordinate
+
+  defstruct guessed?: false, in_island: :none
+
+  def start_link(), do: Agent.start_link(fn -> %Coordinate{} end)
+
+  def guessed?(coordinate),
+    do: Agent.get(coordinate, fn state -> state.guessed? end)
+
+  def in_island(coordinate),
+    do: Agent.get(coordinate, fn state -> state.in_island end)
+
+  def in_island?(coordinate) do
+    case in_island(coordinate) do
+      :none -> false
+      _ -> true
+    end
+  end
+
+  def hit?(coordinate), do: in_island?(coordinate) && guessed?(coordinate)
+
+  def guess(coordinate),
+    do: Agent.update(coordinate, fn state -> Map.put(state, :guessed?, true) end)
+
+  def set_in_island(coordinate, value) when is_atom(value),
+    do: Agent.update(coordinate, fn state -> Map.put(state, :in_island, value) end)
+
+  def to_string(coordinate),
+    do: "(in_island: #{in_island(coordinate)}, guessed: #{guessed?(coordinate)}"
+end
