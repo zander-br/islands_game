cb2bed4df5eb1c5b2bcc1bd602bfab22d0adeb92
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-11-15 18:36:02 -0300

    feat: add set_island_coordinates/3 function in island set agent

--- lib/islands_engine/island_set.ex
+++ lib/islands_engine/island_set.ex
@@ -1,9 +1,17 @@
 defmodule IslandsEngine.IslandSet do
-  alias IslandsEngine.{Island, IslandSet}
+  alias IslandsEngine.{Coordinate, Island, IslandSet}
 
   defstruct atoll: :none, dot: :none, l_shape: :none, s_shape: :none, square: :none
 
   def start_link, do: Agent.start_link(&initialized_set/0)
+
+  def set_island_coordinates(island_set, island_key, new_coordinates) do
+    island = Agent.get(island_set, fn state -> Map.get(state, island_key) end)
+    original_coordinates = Agent.get(island, fn state -> state end)
+    Island.replace_coordinates(island, new_coordinates)
+    Coordinate.set_all_in_island(original_coordinates, :none)
+    Coordinate.set_all_in_island(new_coordinates, island_key)
+  end
 
   def to_string(island_set), do: "%IslandSet{#{string_body(island_set)}}"
 
