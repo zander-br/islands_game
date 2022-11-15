4ee940b3c9465b228f7f78cc7c1e76f53be1a5c8
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-11-15 18:36:48 -0300

    feat: add set_island_coordinates/3 function in player agent

--- lib/islands_engine/player.ex
+++ lib/islands_engine/player.ex
@@ -12,7 +12,26 @@ defmodule IslandsEngine.Player do
   def set_name(player, name),
     do: Agent.update(player, fn state -> Map.put(state, :name, name) end)
 
+  def get_board(player), do: Agent.get(player, fn state -> state.board end)
+
+  def get_island_set(player), do: Agent.get(player, fn state -> state.island_set end)
+
+  def set_island_coordinates(player, island, coordinates) do
+    board = get_board(player)
+    island_set = get_island_set(player)
+    new_coordinates = convert_coordinates(board, coordinates)
+    IslandSet.set_island_coordinates(island_set, island, new_coordinates)
+  end
+
   def to_string(player), do: "%Player{#{string_body(player)}}"
+
+  defp convert_coordinates(board, coordinates),
+    do: Enum.map(coordinates, fn coordinate -> convert_coordinate(board, coordinate) end)
+
+  defp convert_coordinate(board, coordinate) when is_atom(coordinate),
+    do: Board.get_coordinate(board, coordinate)
+
+  defp convert_coordinate(_board, coordinate) when is_pid(coordinate), do: coordinate
 
   defp string_body(player) do
     state = Agent.get(player, & &1)
